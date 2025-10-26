119371866fff3b15bef9995f4c51a588668c7343
Pradeep-Palanichamy <79884050+Pradeep-Palanichamy@users.noreply.github.com>
2025-10-26 18:44:50 +0900

    Update Phyloflash.sh

--- Facultative_bacterial_symbiont_scale_insect_manuscript/Phyloflash.sh
+++ Facultative_bacterial_symbiont_scale_insect_manuscript/Phyloflash.sh
@@ -1,1 +1,7 @@
+# Phyloflash
 
+conda activate pf # If Conda environment not already activated
+phyloFlash.pl -dbhome /path/to/138.1 -lib TEST -CPUs 16 \
+ -read1 ${CONDA_PREFIX}/lib/phyloFlash/test_files/test_R1.fastq.gz \
+ -read2 ${CONDA_PREFIX}/lib/phyloFlash/test_files/test_R2.fastq.gz \
+ -almosteverything
